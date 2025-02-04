ac0057fc1a5bed2b35759ba3fa15f53a8953e60c
rsail <69692454+rsail@users.noreply.github.com>
2025-02-04 16:20:19 -0500

    Initial commit

---
+++ install_extensions.ps1
@@ -0,0 +1,20 @@
+
+# VS Code extensions to install
+$extensions = @(
+    "bierner.emojisense",
+    "michaelCurrin.auto-commit-msg",
+    "ms-python.black-formatter",
+    "github.codespaces",
+    "github.vscode-pull-request-github",
+    "github.github-vscode-theme",
+    "ms-toolsai.jupyter",
+    "esbenp.prettier-vscode",
+    "ms-python.python",
+    "ms-python.debugpy",
+    "ericsia.pythonsnippets3",
+    "ms-vscode.remote-repositories",
+    "emmanuelbeziat.vscode-great-icons"
+)
+foreach ($extension in $extensions) {
+    code --install-extension $extension
+}
